91632e472b84454da7e785999112d994514406a9
Kristen Pérez <102558891+KristenAidee@users.noreply.github.com>
2022-03-31 17:48:52 -0600

    Update Doppler_response.m

--- Doppler_response.m
+++ Doppler_response.m
@@ -22,6 +22,8 @@ xlabel('Speed (m/s)');
 ylabel('Range (m)');
 title('Range-Doppler Map');
 
+% The target is approximately 2250 meters away and is moving fast enough to cause a Doppler shift of approximately -0.357422 Hz.
+
 %% Estimate Doppler and range from range-Doppler response.
 
 % Create a range-Doppler response object.
